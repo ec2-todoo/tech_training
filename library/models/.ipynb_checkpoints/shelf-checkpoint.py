5644e033220fcc4a4358c909f05f3423f51a6221
ec2-todoo <ec@todoo.co>
2020-02-04 23:22:33 +0000

    [ADD] mod11

--- library/models/.ipynb_checkpoints/shelf-checkpoint.py
+++ library/models/.ipynb_checkpoints/shelf-checkpoint.py
@@ -2,6 +2,12 @@
 
 from odoo import models, fields
 
+class LibraryTags(models.Model):
+    _name = 'library.tags'
+    _description ='Library book Tags'
+
+    name = fields.Char(string='Name')
+
 class Library(models.Model):
      _name = 'library.shelf'
      _description = 'Library Shelf'
@@ -10,7 +16,15 @@ class Library(models.Model):
      description = fields.Html(string='Description')
      banner = fields.Binary(string='Banner')
      price = fields.Float(string='Price', digits=(5, 4))
-#     value = fields.Integer()
+
+    expire_date = fields.Date(string='Expire After', required=True)
+    
+    responsible_id = fields.Many2one(comodel_name='res.users', required=True,
+                        string='Responsible', ondelete='restrict', copy=False)
+    
+    tag_ids = fields.Many2many(comodel_name='library.tags', relation='rel_book_tags',
+                            column1='book_id', column2='tag_id', string='Tags')
+    #     value = fields.Integer()
 #     value2 = fields.Float(compute="_value_pc", store=True)
 #     description = fields.Text()
 #
