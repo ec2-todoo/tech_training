be799f3fa45a0463d4c7a29ca7de7d70c79a1d2e
ec2-todoo <60619671+ec2-todoo@users.noreply.github.com>
2020-02-06 10:55:56 -0800

    Add files via upload

---
+++ openacademy3/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "OpenAcademy",
+
+    'summary': """Session, Trainings, Attendance""",
+
+    'description': """
+""",
+    'author': "Odoo, Inc",
+    'website': "https://www.odoo.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Academy',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['contacts'],
+
+    # always loaded
+    'data': [
+        'views/openacademy_course_views.xml',
+        'views/openacademy_session_views.xml',
+        'views/res_partner_views.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        #'demo/openacademy_course_demo.xml',
+    ],
+}
